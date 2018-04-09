2ce5fff471b3a3ae8c053a75a6cc530cef33e587
Juan Pablo L <colo.ljp@hotmail.com>
2018-04-09 19:05:37 -0300

    Added all Nodes to the Controller
    The next thing to do will be add the Controller logic for the User Input Actions

--- src/com/jpl/Controller.java
+++ src/com/jpl/Controller.java
@@ -2,7 +2,7 @@ package com.jpl;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,10 +10,27 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
-    protected ComboBox<String> prioritiesBox;
+    private TableView taskTable;
+
+    @FXML
+    private TextField taskDescriptionField;
+
+    @FXML
+    private ComboBox<String> prioritiesBox;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private CheckBox completedCheckBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         prioritiesBox.getItems().addAll("High", "Medium", "Low");
+
     }
 }
